eb3d6717e82e7b147329111a5f3e5aaf18424fb3
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-28 10:48:20 +0200

    add vehicle methods

--- VehicleApp/Vehicle.cs
+++ VehicleApp/Vehicle.cs
@@ -4,4 +4,14 @@ internal abstract class Vehicle
     public string Manufacturer { get; set; }
     public string Model { get; set; }
     public double Ks { get; set; }
+
+    public double KsToKw()
+    {
+        return Ks * 0.735;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Vehicle details: Manufacturer: {Manufacturer}, Model: {Model}, Ks: {Ks}, Kw: {KsToKw()}");
+    }
 }
