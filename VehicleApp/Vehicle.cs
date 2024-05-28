8899f6bf91cb6862a034637b6bcd8408cbc2f6ab
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-28 10:45:26 +0200

    add vehicle properties

---
+++ VehicleApp/Vehicle.cs
@@ -0,0 +1,7 @@
+﻿namespace VehicleApp;
+internal abstract class Vehicle
+{
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public double Ks { get; set; }
+}
