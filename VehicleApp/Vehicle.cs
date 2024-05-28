2d19347035c3207e2dd037524ac28a71544b89ef
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-28 10:51:02 +0200

    add vehicle constructors

--- VehicleApp/Vehicle.cs
+++ VehicleApp/Vehicle.cs
@@ -1,6 +1,16 @@
 ﻿namespace VehicleApp;
 internal abstract class Vehicle
 {
+    protected Vehicle()
+    {
+    }
+
+    protected Vehicle(string? manufacturer, string? model, double ks)
+    {
+        Manufacturer = manufacturer;
+        Model = model;
+        Ks = ks;
+    }
     public string Manufacturer { get; set; }
     public string Model { get; set; }
     public double Ks { get; set; }
