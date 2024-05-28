5a3150eb939735f1128e0b676d9c9cabc2d54a0a
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-28 12:04:47 +0200

    add aeroplane properties

--- VehicleApp/Aeroplane.cs
+++ VehicleApp/Aeroplane.cs
@@ -2,4 +2,12 @@
 internal class Aeroplane : Vehicle
 {
     public string? Company { get; set; }
+    internal double KsToMw()
+    {
+        return KsToKw() / 1000;
+    }
+    protected override void PrintDetails()
+    {
+        Console.WriteLine($"Vehicle details: Manufacturer: {Manufacturer}, Model: {Model}, Company: {Company} Ks: {Ks}, Kw: {KsToKw()}");
+    }
 }
