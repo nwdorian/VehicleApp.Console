e8353d2f68bed0482438b20a009f07da5aab1a91
Dorian Neuwirth <neuwirthdorian@gmail.com>
2024-05-28 11:09:16 +0200

    add car methods

--- VehicleApp/Car.cs
+++ VehicleApp/Car.cs
@@ -2,4 +2,12 @@
 internal class Car : Vehicle
 {
     public int Ccm { get; set; }
+    protected override double KsToKw()
+    {
+        return Ks * 0.735;
+    }
+    protected override void PrintDetails()
+    {
+        Console.WriteLine($"Vehicle details: Manufacturer: {Manufacturer}, Model: {Model}, Displacement: {Ccm} Ks: {Ks}, Kw: {KsToKw()}");
+    }
 }
